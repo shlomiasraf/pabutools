19a3b996b11a6428356ac4c012928b86134a54ad
Simon-Rey <reysimon@orange.fr>
2024-11-27 12:09:20 +0100

    Updating tests from URL

--- tests/test_pabulib.py
+++ tests/test_pabulib.py
@@ -679,9 +679,10 @@ voter_id;vote;voting_method;district
             f.write(
                 """META
 key;value
-description;Local PB in Warsaw, Ochota | Pole Mokotowskie
+description;Local PB in Warszawa, Ochota | Pole Mokotowskie
 country;Poland
 unit;Warszawa
+district;Ochota
 subunit;Pole Mokotowskie
 instance;2018
 num_projects;1
@@ -691,25 +692,25 @@ vote_type;approval
 rule;greedy
 date_begin;14.06.2017
 date_end;30.06.2017
-language;pl
-edition;4
-district;Ochota
 min_length;1
 max_sum_cost;21064
+edition;4
+language;pl
 currency;PLN
 PROJECTS
-project_id;cost;name;category;target;votes;selected
-658;21064;Dobre grzyby (mikoryza) dla drzew Pola Mokotowskiego;environmental protection,public space,urban greenery;seniors,families with children,animals;8;1
+project_id;cost;votes;name;category;target;selected
+658;21064;8;Dobre grzyby (mikoryza) dla drzew Pola Mokotowskiego;environmental protection,public space,urban greenery;seniors,families with children,animals;1
 VOTES
-voter_id;age;sex;voting_method;vote
-30397;43;F;internet;658
-39076;61;F;paper;658
-56422;66;M;internet;658
-67344;32;F;internet;658
-83776;73;F;internet;658
-89587;9;M;internet;658
-95332;31;F;internet;658
-102415;24;F;internet;658"""
+voter_id;vote;age;sex;voting_method
+30397;658;43;F;internet
+39076;658;61;F;paper
+56422;658;66;M;internet
+67344;658;32;F;internet
+83776;658;73;F;internet
+89587;658;9;M;internet
+95332;658;31;F;internet
+102415;658;24;F;internet
+"""
             )
 
         file_inst, file_prof = parse_pabulib("test.pb")
